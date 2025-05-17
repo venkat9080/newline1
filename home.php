801ae31572faff0593da4f7bd1a10bf19faee41a
venkat9080 <83752109+venkat9080@users.noreply.github.com>
2025-05-17 18:01:01 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,363 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Responsive Furniture Landing Page</title>
+    <link rel="icon" type="image/png" href="store.png">
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+    <link rel="stylesheet" href="style.css">
+    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+
+</head>
+<body>
+    <header>
+        <a href="#" class="logo">Shop</a>
+        <i class='bx bx-menu' id="menu-icon"></i>
+
+        <ul class="navbar">
+            <li><a href="#home">Home</a></li>
+            <li><a href="#shop">Shop</a></li>
+            <li><a href="#new">New Arrivals</a></li>
+            <li><a href="#about">About</a></li>
+            <li><a href="#brands">Our Partners</a></li>
+            <li><a href="#contact">Contact</a></li>
+        </ul>
+    </header>
+    <div data-aos="fade-down"> <section class="home" id="home">
+        <div class="home-text">
+            <h1>Your <span>Comfort</span> <br> Is Our <span>Happiness</span></h1>
+            <p>We create an environment where you can truly relax and be yourself</p>
+            <a href="#shop" class="btn">Shop Now</a>
+        </div>
+    </section></div>
+   
+    <div data-aos="zoom-out-left"><section class="shop" id="shop">
+        <div class="heading">
+            <span>New Arrival</span>
+            <h2>Shop Now</h2>
+        </div>
+
+        <div class="shop-container">
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/ZntRdfd1/p1.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Gray Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$45</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/c1zGvCVw/p2.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Blue Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$50</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/T3M8cS2P/p3.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Pink Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$60</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/JnhSnCxp/p4.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Tripod Gray Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$40</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+             <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/0N59p3Zw/p5.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Tripod Black Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$45</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/gJQdb0pV/p6.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Black Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$50</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+        </div>
+    </section></div>
+    
+    <div data-aos="flip-left">
+    <section class="new" id="new">
+        <div class="heading">
+            <span>New Collection</span>
+            <h2>Best Selling</h2>
+        </div>
+
+        <div class="new-container">
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/1tMFt9L8/new1.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Rustic Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$30</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/7Yj2btQn/new2.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>Bright Yellow Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$40</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+
+            <div class="box">
+                <div class="box-img">
+                    <img src="https://i.postimg.cc/wvWtzwmJ/new3.jpg" alt="image">
+                </div>
+
+                <div class="title-price">
+                    <h3>White Chair</h3>
+                    <div class="stars">
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star' ></i>
+                        <i class='bx bxs-star-half' ></i>
+                    </div>
+                </div>
+                <span>$35</span>
+                <i class='bx bx-cart' ></i>
+            </div>
+        </div>
+    </section>
+    </div>
+    
+    <div data-aos="flip-down"><section class="about" id="about">
+        <div class="about-img">
+            <img src="https://i.postimg.cc/jjtCJp6P/about.jpg" alt="image">
+        </div>
+
+        <div class="about-text">
+            <span>About us</span>
+            <h2>Furniture is a important part <br>for your comfort</h2>
+            <p>We spend a significant portion of our lives within the walls of our homes. It's where we recharge, relax, and create memories</p>
+            <p>Investing in quality furniture is an investment in your comfort and happiness. It's about creating a space that reflects your personality and nourishes your soul</p>
+            <a href="#shop" class="btn">Learn More</a>
+        </div>
+    </section></div>
+    
+
+   
+    <div data-aos="flip-down"> <section class="brands" id="brands">
+        <div class="heading">
+            <span>Brands</span>
+            <h2>Our Brand Partners</h2>
+        </div>
+
+        <div class="brands-container">
+            <script>$('.autoplay').slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});</script>
+        
+            <img src="https://i.postimg.cc/JzPtZTpF/Google.png" alt="brand image">
+            <img src="https://i.postimg.cc/sxYgXKBw/amazon.png" alt="brand image">
+            <img src="https://i.postimg.cc/GhRpN0NN/netflix.png" alt="brand image">
+            <img src="https://i.postimg.cc/jSgdLD50/tesla.png" alt="brand image">
+            <img src="https://i.postimg.cc/hGX4xKzJ/starbucks.png" alt="brand image">
+            <img src="https://i.postimg.cc/G3FLv72j/zoom.png" alt="brand image">
+        </div>
+    </section></div>
+    <div data-aos="flip-down"><section class="newsletter" id="contact">
+        <h2>Subscribe to Newsletter</h2>
+        <div class="news-box">
+    <form action="" method="POST">
+        <input type="text" name="name" placeholder="Name" required>
+        <input type="email" name="email" placeholder="Enter your email" required>
+        <input type="number" name="phone" placeholder="Enter your phone" required>
+        <input type="text" name="message" placeholder="Message" required>
+        <button type="submit" class="btn">Subscribe</button>
+    </form>
+</div>
+
+    </section></div>
+    <div data-aos="flip-down"><section class="footer" id="footer">
+        <div class="footer-box">
+            <h2>Brand <span>X</span></h2>
+            <p>Invest in your comfort and happiness.</p>
+            <div class="social">
+                <a href="#"><i class='bx bxl-facebook'></i></a>
+                <a href="#"><i class='bx bxl-twitter'></i></a>
+                <a href="#"><i class='bx bxl-instagram'></i></a>
+            </div>
+        </div>
+
+        <div class="footer-box">
+            <h3>Services</h3>
+            <li><a href="#">Product</a></li>
+            <li><a href="#">Help & Support</a></li>
+            <li><a href="#">Pricing</a></li>
+            <li><a href="#">FAQ</a></li>
+        </div>
+
+        <div class="footer-box">
+            <h3>Product</h3>
+            <li><a href="#">Sofa's</a></li>
+            <li><a href="#">Chair's</a></li>
+            <li><a href="#">Living Room</a></li>
+            <li><a href="#">Office</a></li>
+        </div>
+
+        <div class="footer-box contact-info">
+            <h3>Contact</h3>
+            <span>Raja street chengam</span>
+            <span>9080131768</span>
+            <span>venkatesh@gmail.com</span>
+        </div>
+    </section></div>
+    
+
+    
+
+    
+
+    
+    <script src="main.js"></script>
+</body>
+</html>
+
+
+<?php
+// Database connection
+$host = "localhost";
+$db = "newsletter_db";
+$user = "root";
+$pass = "";
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$db", $user, $pass);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // Get POST data
+    $name = $_POST['name'] ?? '';
+    $email = $_POST['email'] ?? '';
+    $phone = $_POST['phone'] ?? '';
+    $message = $_POST['message'] ?? '';
+
+    // Check if email already exists
+    $checkStmt = $pdo->prepare("SELECT COUNT(*) FROM subscribers WHERE email = ?");
+    $checkStmt->execute([$email]);
+    $count = $checkStmt->fetchColumn();
+
+    if ($count > 0) {
+        echo "You have already subscribed with this email.";
+    } else {
+        // Insert into database
+        $stmt = $pdo->prepare("INSERT INTO subscribers (name, email, phone, message) VALUES (?, ?, ?, ?)");
+        $stmt->execute([$name, $email, $phone, $message]);
+
+        echo "Thank you for subscribing!";
+    }
+} catch (PDOException $e) {
+    echo "Error: " . $e->getMessage();
+}
+?>
+
+<script>
+  AOS.init();
+</script>
